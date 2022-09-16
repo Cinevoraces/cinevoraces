4e1e7ec606df29acd7bbb7a590d69f188a6fe170
Julien GOLETTO <julien.goletto@gmail.com>
2022-09-16 22:09:10 +0200

    :heavy_plus_sign: Headless UI added

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -6,7 +6,6 @@ const Home: NextPage = () => {
   return (
     <main>
       <h1 className='text-3xl font-bold text-orange-primary'>Bonjour et bienvenue chez Cinévoraces !</h1>
-      
     </main>
   );
 };
