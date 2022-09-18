1e3b813711a5886b978df3e3063bccae7ae82947
benoitSafari <benoit.safari.officiel@gmail.com>
2022-09-18 14:40:53 +0200

    :heavy_plus_sign: Installed qs package
    Installed qs package

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,12 @@
 import 'module-alias/register';
 import Fastify from 'fastify';
+import qs from 'qs';
 import app from '@src/app';
 
-const fastify = Fastify({logger: true});
+const fastify = Fastify({
+  logger: false,
+  querystringParser: str => qs.parse(str)
+});
 const serverConfig = {
   port: Number(process.env.port) || 8080,
   host: '0.0.0.0'
