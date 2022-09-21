ba46127ba00f877179e40ec8babd752f48d6c262
anthonyespirat <jayshu666@gmail.com>
2022-09-21 23:37:07 +0200

    ✨ Movies route

--- api/src/controllers/movie.controllers.ts
+++ api/src/controllers/movie.controllers.ts
@@ -1,10 +1,31 @@
+
+import Filters from '@src/types/Filters';
+import movieFiltersFactory from '@src/utils/movieFiltersFactory';
 import { FastifyReply, FastifyRequest } from 'fastify';
 
-export const getMovies = async (request: FastifyRequest, reply: FastifyReply) => {
+type Request = FastifyRequest<{
+  Querystring: {
+    filter: Filters.Movie
+  }
+}>
+
+type Reply = FastifyReply
+
+
+export const getMovies = async (request: Request, reply: Reply) => {
   const { prisma } = request;
 
   try {
-    const movies = await prisma.movie.findMany();
+    const filters = movieFiltersFactory(request.query.filter)
+    
+    const movies = await prisma.movie.findMany({
+      where: {
+        AND: [
+          ...filters
+        ]
+      }
+
+    });
     reply.send(movies);
     
   } catch (error) {
--- api/src/routes/movie.route.ts
+++ api/src/routes/movie.route.ts
@@ -3,9 +3,18 @@ import { getMovies } from "@src/controllers/movie.controllers";
 export const movie = async (fastify: FastifyInstance) => {
 
   const movieGetOpts = {
-    schema: {},
+    schema: {
+      querystring: {
+        type: "object",
+        properties: {
+          filter: {
+            type: 'object'
+          }
+        }
+      }
+    },
     handler: getMovies
   }
 
-  fastify.get('/', movieGetOpts);
+  fastify.get('/movies', movieGetOpts);
 }
