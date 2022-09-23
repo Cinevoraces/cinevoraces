ae65a246c7e7693985e78c673161be82cddb13de
anthonyespirat <jayshu666@gmail.com>
2022-09-23 14:08:46 +0200

    🧱 change server build

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,18 +4,24 @@ import app from '@src/app';
 import qs from "qs"
 import parseOptions from '@src/utils/parseOptions';
 
-const fastify = Fastify({logger: true, querystringParser: (str) => qs.parse(str, parseOptions)});
+
 
 const serverConfig = {
   port: Number(process.env.port) || 8080,
   host: '0.0.0.0'
 }
 
-fastify.register(app);
+export default async function createServer () {
+  try {
+    const fastify = Fastify({logger: true, querystringParser: (str) => qs.parse(str, parseOptions)});
+    fastify.register(app);
 
-fastify.listen({...serverConfig}, err => {
-  if (err) {
-    fastify.log.error(err);
+    fastify.listen({...serverConfig})
+
+  } catch (err) {
+    console.error(err);
     process.exit(1);
   }
-});
+}
+
+createServer()
