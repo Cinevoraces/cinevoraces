0bf9726c3654917bd6b8dca6c823d1170c048bcf
BenoitSafari <gregory.michalak.192@gmail.com>
2022-09-23 17:21:19 +0200

    :bug: fixed wrong setup on jwt plugin

--- api/src/plugins/jwt.ts
+++ api/src/plugins/jwt.ts
@@ -1,20 +1,19 @@
-import type { FastifyReply, FastifyRequest } from 'fastify';
-import fastifyJwt from '@fastify/jwt'
-import plugin from 'fastify-plugin';
+import fastifyJwt, { FastifyJWTOptions } from "@fastify/jwt";
+import plugin from "fastify-plugin";
 
 const jwt = async (fastify: fastifyInstance) => {
+  if (fastify.jwt) {
+    return fastify.log.warn("Fastify/jwt already registered");
+  }
+
   fastify.register(fastifyJwt, {
-    secret: process.env.JWT_SECRET
-  })
-  
-  fastify
-    .decorate('authenticate', async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      await request.jwtVerify()
-    } catch (err) {
-      reply.send(err)
-    }
+    secret: process.env.JWT_SECRET,
   });
 };
 
-export default plugin(jwt);
+export default plugin<FastifyJWTOptions>(jwt);
+
+// handler's request object now has following methods:
+// jwtSign()
+// jwtVerify()
+// jwtDecode()
