e89b13afd8e0e537020fb642cb64e7591e179cd5
BenoitSafari <gregory.michalak.192@gmail.com>
2022-09-24 18:48:28 +0200

    :recycle: Code cleanup

--- api/src/modules/auth/auth.handler.ts
+++ api/src/modules/auth/auth.handler.ts
@@ -23,24 +23,29 @@ export const register = async (request: Request, reply: Reply) => {
 
     if (user) {
       // French messages are to be displayed in front
-      if (mail === user.mail)
+      if (mail === user.mail) {
         throw new Error("Cette adresse mail est déjà associée à un compte.");
-      if (pseudo === user.pseudo)
+      }
+      if (pseudo === user.pseudo) {
         throw new Error("Ce pseudo est déjà utilisé.");
+      }
     }
   } catch (error) {
-    // 409: Conflict
-    reply.code(409).send(error);
+    return reply
+      .code(409) // Conflict
+      .send(error);
   }
 
   // Test and Hash password
   if (!process.env.PASS_REGEXP) {
-    // 500: Internal Server Error
-    reply.code(500).send(new Error("No password regexp found."));
+    return reply
+      .code(500) // Internal Server Error
+      .send(new Error("No password regexp found."));
   }
   if (!password.match(process.env.PASS_REGEXP)) {
-    // 422: Unprocessable Entity
-    reply.code(422).send(new Error("Invalid password."));
+    return reply
+      .code(422) // Unprocessable Entity
+      .send(new Error("Invalid password."));
   }
   const salt = await bcrypt.genSalt(10);
   password = await bcrypt.hash(password, salt);
@@ -54,10 +59,8 @@ export const register = async (request: Request, reply: Reply) => {
         password,
       },
     });
-
-    // 201: Created
     reply
-      .code(201)
+      .code(201) // Created
       .send({ message: `Utilisateur "${pseudo}" créé avec succés.` });
   } catch (error) {
     reply.send(error);
