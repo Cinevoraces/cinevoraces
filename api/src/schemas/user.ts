6b9fffae6810e6eed5f4588444ff5eec0ae206c7
benoitSafari <benoit.safari.officiel@gmail.com>
2022-09-25 23:10:25 +0200

    :construction: added user/review schemas

---
+++ api/src/schemas/user.ts
@@ -0,0 +1,50 @@
+const userSchema = {
+  $id: "user",
+  type: "object",
+  properties: {
+    id: {
+      type: "number",
+    },
+    pseudo: {
+      type: "string",
+    },
+    mail: {
+      type: "string",
+    },
+    avatar_url: {
+      type: "string",
+    },
+    role: {
+      type: "string",
+    },
+    created_at: {
+      type: "string",
+    },
+    updated_at: {
+      type: "string",
+    },
+    movies: {
+      type: "array",
+      items: {
+        items: { $ref: "movie#" },
+      },
+    },
+    reviews: {
+      type: "array",
+      items: {
+        items: { $ref: "review#" },
+      },
+    },
+  },
+  required: [
+    "id",
+    "pseudo",
+    "mail",
+    "avatar_url",
+    "role",
+    "created_at",
+    "updated_at",
+  ],
+};
+
+export default userSchema;
