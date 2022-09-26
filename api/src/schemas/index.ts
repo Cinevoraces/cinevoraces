92b76d1aa542b50d2d97a4ed0f018116d0354461
benoitSafari <benoit.safari.officiel@gmail.com>
2022-09-26 10:26:03 +0200

    :bug: Added all schemas to schema registering function

--- api/src/schemas/index.ts
+++ api/src/schemas/index.ts
@@ -1,9 +1,12 @@
 import type { FastifyPluginCallback } from "fastify";
 import movieSchema from "./movie";
+import userSchema from "./user";
+import reviewSchema from "./review";
+
 import plugin from "fastify-plugin";
 
 const schemasRegister: FastifyPluginCallback = async (fastify, opts, done) => {
-  const schemas = [movieSchema];
+  const schemas = [movieSchema, userSchema, reviewSchema];
 
   schemas.forEach((schema) => {
     fastify.addSchema(schema);
