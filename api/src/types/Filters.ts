4544f929e4a4c26798710d099dc3e50181fbdb85
anthonyespirat <jayshu666@gmail.com>
2022-10-08 06:32:17 +0200

    ✨ Add orderBy & pagination
    did not use lastest, use   order then  pagination

--- api/src/types/Filters.ts
+++ api/src/types/Filters.ts
@@ -1,7 +1,7 @@
 import type { movie, proposition_slot } from '@prisma/client';
 
 export declare namespace Filters {
-  type Movie = Pick<movie, 'is_published' | 'season_id' | 'user_id'>;
+  type Movie = Pick<movie, 'is_published' | 'season_id' | 'user_id'> & { orderBy: 'asc' | 'desc', pagination: number };
   type Slot = Pick<proposition_slot, 'is_booked'>;
   interface User {
     'metrics': boolean, 
