403a1bad6906457268ca3df6ab17c7129f83f039
BenoitSafari <gregory.michalak.192@gmail.com>
2022-10-26 00:59:58 +0200

    :label: Exported types from DB and removed useless files

--- api/src/types/Metrics.ts
+++ api/src/types/Metrics.ts
@@ -1,13 +1,17 @@
-export interface globalMetrics {
-  seasons_count: number;
-  movies_count: number;
-  countries_count: number;
+declare namespace Metrics {
+  interface global {
+    seasons_count: number;
+    movies_count: number;
+    countries_count: number;
+  }
+  interface user {
+    id?: number;
+    propositions_count: number;
+    comments_count: number;
+    likes_count: number;
+    watchlist_count: number;
+    ratings_count: number;
+  }
 }
-export interface userMetrics {
-  id?: number;
-  propositions_count: number;
-  comments_count: number;
-  likes_count: number;
-  watchlist_count: number;
-  ratings_count: number;
-}
+
+export type { Metrics };
--- api/src/types/Query.ts
+++ api/src/types/Query.ts
@@ -1,34 +1,12 @@
-declare namespace PrismaQuery {
-  interface Querystring {
+declare namespace Query {
+  interface querystring {
     filter?: Record<string, unknown>;
     pop?: Record<string, unknown>;
     limit?: number;
     asc?: string;
     desc?: string;
   }
-  type FactoredQuery = where & include & take & orderBy;
 
-  interface where {
-    where?: { 
-      AND?: Array<Record<string, unknown>>;
-      review?: {
-        some?: Record<string, unknown>;
-      }
-    }
-  }
-  interface include {
-    include?: {
-      [key: string]: unknown;
-    }
-  }
-  interface take {
-    take?: number;
-  }
-  interface orderBy {
-    orderBy?: {
-      [key: string]: 'asc' | 'desc';
-    }
-  }
 }
 
-export default PrismaQuery;
+export default Query;
