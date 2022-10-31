73008f53c01dec16c92bd6b5e54f74963ec953a8
benoitSafari <benoit.safari.officiel@gmail.com>
2022-10-31 10:23:33 +0100

    :white_check_mark: Added tests on getUsers

--- api/test/tests_routes/Users.test.ts
+++ api/test/tests_routes/Users.test.ts
@@ -23,44 +23,88 @@ describe('USERS ENDPOINTS', () => {
   };
 
   test('GET USERS', async () => {
+    // GET ALL USERS WITHOUT POPULATING
     const getDefaultUsers = await app.inject(inject.getUsers);
     expect(await getDefaultUsers.json()).toEqual(expect.arrayContaining([expectedObjects.user]));
 
+    // GET ALL USERS WITH REVIEWS
     const getUsersWithReviews = await app.inject({
       ...inject.getUsers,
       url: '/users?select[reviews]=true'
     });
     expect(await getUsersWithReviews.json()).toEqual(expect.arrayContaining([expectedObjects.userWithReviews]));
 
+    // GET ALL USERS WITH PROPOSITIONS, METRICS AND REVIEWS
     const getUsersWithMoviesReviewsAndMetrics = await app.inject({
       ...inject.getUsers,
       url: '/users?select[reviews]=true&select[metrics]=true&select[propositions]=true'
     });
     expect(await getUsersWithMoviesReviewsAndMetrics.json()).toEqual(expect.arrayContaining([expectedObjects.userWithMoviesReviewsAndMetrics]));
 
+    // TESTING "false" STATEMENTS
     const getUsersWithPopulatorsSetToFalse = await app.inject({
       ...inject.getUsers,
       url: '/users?select[reviews]=false&select[metrics]=false&select[propositions]=false'
     });
     expect(await getUsersWithPopulatorsSetToFalse.json()).toEqual(expect.arrayContaining([expectedObjects.user]));
 
+    // GET USER(S) BY ID
     const getUsersFilteredById = await app.inject({
       ...inject.getUsers,
       url: '/users?where[id]=1'
     });
-
     expect(await getUsersFilteredById.json().length).toEqual(1);
     expect(await getUsersFilteredById.json()[0]).toEqual(expect.objectContaining({ id: 1 }));
+
+    // GET USER(S) BY PSEUDO
+    const getUsersFilteredByPseudo = await app.inject({
+      ...inject.getUsers,
+      url: `/users?where[pseudo]=${res.users[0].user.pseudo}`
+    });
+    expect(await getUsersFilteredByPseudo.json().length).toEqual(1);
+    expect(await getUsersFilteredByPseudo.json()[0]).toEqual(expect.objectContaining({ pseudo: res.users[0].user.pseudo }));
+
+    // GET USER(S) BY MAIL
+    const getUsersFilteredByMail = await app.inject({
+      ...inject.getUsers,
+      url: `/users?where[mail]=${res.users[0].user.mail}`
+    });
+    expect(await getUsersFilteredByMail.json().length).toEqual(1);
+    expect(await getUsersFilteredByMail.json()[0]).toEqual(expect.objectContaining({ mail: res.users[0].user.mail }));
+
+    // GET USER(S) BY ROLE
+    const getUsersFilteredByRole = await app.inject({
+      ...inject.getUsers,
+      url: '/users?where[role]=user'
+    });
+    expect(await getUsersFilteredByRole.json()[0]).toEqual(expect.objectContaining({ role: 'user' }));
+
+    // GET ONE USER WITH REVIEWS
+    const getOneUserWithReviews = await app.inject({
+      ...inject.getUsers,
+      url: '/users?where[id]=1&select[reviews]=true'
+    });
+    expect(await getOneUserWithReviews.json().length).toEqual(1);
+    expect(await getOneUserWithReviews.json()).toEqual(expect.arrayContaining([expectedObjects.userWithReviews]));
+
+    // GET '404' WHEN NO RESULTS
+    const get404WhenNoResults = await app.inject({
+      ...inject.getUsers,
+      url: '/users?where[pseudo]=admin&where[id]=1'
+    });
+    expect(get404WhenNoResults.statusCode).toEqual(404);
+
   });
 
-  // test('DELETE USERS', async () =>{
-  //   inject.login = {
-  //     ...inject.login, 
-  //     payload: { pseudo: res.users[0].user.pseudo, password: res.users[0].user.password },
-  //   };
-  //   const login = await app.inject({
-  //     ...inject.login,
-  //     payload: { pseudo: res.users[0].user.pseudo, password: res.users[0].user.password },
-  //   });
-  // });
+  test('DELETE USERS', async () =>{
+    // LOG ADMIN
+    inject.login = {
+      ...inject.login, 
+      payload: { pseudo: res.users[0].user.pseudo, password: res.users[0].user.password },
+    };
+    const login = await app.inject({
+      ...inject.login,
+      payload: { pseudo: res.users[0].user.pseudo, password: res.users[0].user.password },
+    });
+  });
 });
