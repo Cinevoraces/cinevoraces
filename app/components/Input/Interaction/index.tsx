4c8730f99435668acb7fa35774919aabdc5f01d1
YvesSignal <94864153+Julien-Goletto@users.noreply.github.com>
2022-12-08 10:53:53 +0100

    Update app/components/Input/Interaction/index.tsx
    Remove React import
    
    Co-authored-by: BenoitSafari <88612813+BenoitSafari@users.noreply.github.com>

--- app/components/Input/Interaction/index.tsx
+++ app/components/Input/Interaction/index.tsx
@@ -1,5 +1,5 @@
 import type { FormEvent } from 'react';
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import { BookmarkSvg, LikeSvg, StarSvg, ViewSvg } from './SVG';
 import StarRadio from './StarRadio';
 import useCloseMenuOnOutsideClick from '@hooks/useCloseMenuOnOutsideClick';
