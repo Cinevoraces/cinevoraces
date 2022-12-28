864ad6bc5b46fd3b49d40cf538360ad24136c7e0
Julien GOLETTO <julien.goletto@gmail.com>
2022-12-28 16:29:22 +0100

    :sparkles: Error page achieved

---
+++ app/components/SvgComponents/index.ts
@@ -0,0 +1,13 @@
+import ErrorSvg from './Error';
+import { FilterSvg, ResetSvg } from './Filter';
+import { BookmarkSvg, LikeSvg, StarSvg, ViewSvg } from './InteractionsSVG';
+import SimpleLogo from './SimpleLogo';
+import UnwatchedSvg from './Unwatched';
+
+export { 
+  ErrorSvg,
+  FilterSvg, ResetSvg,
+  BookmarkSvg, LikeSvg, StarSvg, ViewSvg,
+  SimpleLogo,
+  UnwatchedSvg,
+};
