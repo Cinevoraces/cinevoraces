be1ae9567048a162224007f761109e21b985f238
Julien GOLETTO <julien.goletto@gmail.com>
2022-12-28 16:34:03 +0100

    :sparkles: adding movie posters

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -1,12 +1,13 @@
 import { ErrorSvg } from '@components/SvgComponents';
 import { useRouter } from 'next/router';
 import { Button } from '@components/Input';
+import PostersComponent from '@components/PostersComponent';
 
 export default function Custom404() {
   const router = useRouter();
   const path = router.asPath;
   return (
-    <main className="custom-container">
+    <main className="custom-container flex flex-col lg:flex-row">
       <div className='w-full flex flex-col items-center gap-16'>
         <h1 className='hero-text'>Erreur 404</h1>
         <p>{'L\'adresse '}<span className='emphasis'>https://cinevoraces{path}</span>{' n\'a rien donné.'}</p>
@@ -17,6 +18,7 @@ export default function Custom404() {
           </Button>
         </div>
       </div>
+      <PostersComponent number={3}/>
     </main>
   );
 };
