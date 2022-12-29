b45e81fa4e297c7a20a92ad20844015b59c96ff3
Julien GOLETTO <julien.goletto@gmail.com>
2022-12-29 19:54:44 +0100

    :bug: Corrected empty seasons behaviour

--- app/custom_types/index.ts
+++ app/custom_types/index.ts
@@ -1,5 +1,6 @@
 import type { MinimalMovie, Presentation, Metrics, Comment, UserReview, CompleteMovie } from './movies';
-import type { Season, FilterOptions, FilterUserInputs, FilteredMoviesStateInterface } from './filmsPage';
+import type Season from './season';
+import type { SeasonOption, FilterOptions, FilterUserInputs, FilteredMoviesStateInterface } from './filmsPage';
 import type { SvgProps } from './global';
 import type { EpisodeOption, MovieBody } from './propositionPage';
 import type { TMDBMovie, TMDBDetailedMovie, TMDBMovieCredits } from './tmdbMovie';
@@ -7,7 +8,8 @@ import type { Episode } from './episodes';
 
 export type{
   MinimalMovie, Presentation, Metrics, Comment, UserReview, CompleteMovie,
-  Season, FilterOptions, FilterUserInputs, FilteredMoviesStateInterface,
+  Season,
+  SeasonOption, FilterOptions, FilterUserInputs, FilteredMoviesStateInterface,
   SvgProps,
   EpisodeOption, MovieBody,
   TMDBMovie, TMDBDetailedMovie, TMDBMovieCredits,
--- app/pages/films/index.tsx
+++ app/pages/films/index.tsx
@@ -17,7 +17,7 @@ import {
   setFilteredMovies,
   cleanUserInputs,
 } from '@store/slices/filteredMovies';
-import type { CompleteMovie, Season, FilterOptions } from '@custom_types/index';
+import type { CompleteMovie, Season } from '@custom_types/index';
 import type { ChangeEvent } from 'react';
 
 import filtersSync from '@utils/filterSyncer';
@@ -43,6 +43,7 @@ const metadatas = [
 ];
 let selectQueryString: string = '';
 metadatas.forEach((dataName) => (selectQueryString += `&select[${dataName}]=true`));
+console.log(selectQueryString);
 
 export default function Films() {
   const dispatch = useAppDispatch();
@@ -50,7 +51,8 @@ export default function Films() {
   const { season, searchQuery, isSeasonSelectOpened } = useAppSelector(filteredMovies);
   const handleToggleSeasonSelect = () => dispatch(toggleSeasonSelect());
   const handleSeasonChange = (season: Season) => dispatch(changeSeason(season));
-  const handleChangeSearchValue = (e: ChangeEvent) => (e.currentTarget instanceof HTMLInputElement) && dispatch(changeSearchQuery(e.currentTarget.value));
+  const handleChangeSearchValue = (e: ChangeEvent) =>
+    e.currentTarget instanceof HTMLInputElement && dispatch(changeSearchQuery(e.currentTarget.value));
   //Filter state mechanics
   const { isFilterMenuOpen, availableFilters, userFilterInputs } = useAppSelector(filteredMovies);
   const handleToggleFilterMenu = () => dispatch(toggleFilterMenu());
@@ -58,7 +60,7 @@ export default function Films() {
   const handleFilterReset = () => dispatch(cleanUserInputs());
 
   const { data: seasonsArray } = useSWR('/seasons');
-  const seasons = useRef<{ name: string; value: string; }[]>([]);
+  const seasons = useRef<{ name: string; value: string }[]>([]);
 
   const isUserConnected = useRef<boolean>(false);
   const id = useAppSelector(user).id;
@@ -68,52 +70,66 @@ export default function Films() {
 
   // Initialise the default value for select button and first movies fetching
   useEffect(() => {
-    if (seasonsArray){
-      seasons.current = [ 
+    if (seasonsArray) {
+      seasons.current = [
         ...seasonsArray
-          .map( (s: { season_number: number; year: number; movie_count: number }) => (
-            {
-              name: `Saison ${s.season_number} - ${s.year}`,
-              value: '' + s.season_number,
-            }
-          )),
-        { name: 'Tous les films', value: '0' }]
-        .sort((a, b) => a.value > b.value ? -1 : 1);
+          .filter((s: Season) => s.movie_count > 0)
+          .map((s: { season_number: number; year: number; movie_count: number }) => ({
+            name: `Saison ${s.season_number} - ${s.year}`,
+            value: '' + s.season_number,
+          })),
+        { name: 'Tous les films', value: '0' },
+      ].sort((a, b) => (a.value > b.value ? -1 : 1));
     }
-    (seasonsArray && !season) && dispatch(changeSeason(seasons.current[0]));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    seasonsArray && !season && dispatch(changeSeason(seasons.current[0]));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [seasonsArray]);
   // Recovers movies from asked season, once season is defined
-  const { data: movies, error, mutate } = useSWR(() => (season) && `/movies?where[is_published]=true&${selectQueryString}${(season.value !== '0') ? `&where[season_number]=${season.value}` : ''}`);
+  const {
+    data: movies,
+    error,
+    mutate,
+  } = useSWR(
+    () =>
+      season &&
+      '/movies?where[is_published]=true' +
+        `${selectQueryString}${season.value !== '0' ? `&where[season_number]=${season.value}` : ''}`
+  );
   // Each Season change triggers an SWR call
   // Changing movie set alters filters displayed and stored in state
   // then reapply current filter rules to the new movie set
   useEffect(() => {
     mutate();
-    if (movies){
+    if (movies) {
       dispatch(setAvailableFilters(filtersSync(movies)));
       dispatch(initializeOrCorrectUserInputs());
-      // movies && dispatch(setFilteredMovies({ filteredMovies: movies, isUserConnected }));  
+      // movies && dispatch(setFilteredMovies({ filteredMovies: movies, isUserConnected }));
+    } else {
+      dispatch(setAvailableFilters(filtersSync([])));
+      dispatch(initializeOrCorrectUserInputs());
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [movies]);
 
   useEffect(() => {
     movies && dispatch(setFilteredMovies({ moviesToFilter: movies, isUserConnected: isUserConnected.current }));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [movies, searchQuery, userFilterInputs, isUserConnected.current]);
 
   const movieResults = useAppSelector(filteredMovies).filteredMovies;
 
   return (
     <main className="custom-container justify-start">
-      <section className='w-full'>
-        <h1 className='hero-text text-start mb-4'>Les films de la communauté</h1>
-        <p>Retrouvez saison par saison les films sélectionnés par les <span className='emphasis'>membres de CinéVoraces</span>.
-        Chaque saison correspond à une année calendaire.<br/>
-        Bonnes découvertes !</p>
+      <section className="w-full">
+        <h1 className="hero-text text-start mb-4">Les films de la communauté</h1>
+        <p>
+          Retrouvez saison par saison les films sélectionnés par les{' '}
+          <span className="emphasis">membres de CinéVoraces</span>. Chaque saison correspond à une année calendaire.
+          <br />
+          Bonnes découvertes !
+        </p>
       </section>
-      <section className='w-full flex flex-col gap-6 align-start md:flex-row'>
+      <section className="w-full flex flex-col gap-6 align-start md:flex-row">
         {season && (
           <SearchBar
             name="searchbarSelect"
@@ -125,33 +141,32 @@ export default function Films() {
             customStyle="searchbar"
             id="search"
             placeholder="🔍 Rechercher un film"
-            value={(searchQuery) ? searchQuery : ''}
+            value={searchQuery ? searchQuery : ''}
             onChange={handleChangeSearchValue}
           />
         )}
-        { (availableFilters && movieResults) &&
-        <Filter 
-          isMenuOpened={isFilterMenuOpen}
-          displayMenuSetter={handleToggleFilterMenu}
-          filterOptions={availableFilters}
-          userFilterInputs={userFilterInputs}
-          userFilterInputsSetter={handleSetFilters}
-          userFilterReset={handleFilterReset}
-          filtersCounter={Number(userFilterInputs.filtersCounter[0])}
-          resultsCount={movieResults.length}
-          isUserConnected={isUserConnected.current}
-        />
-        }
+        {availableFilters && movieResults && (
+          <Filter
+            isMenuOpened={isFilterMenuOpen}
+            displayMenuSetter={handleToggleFilterMenu}
+            filterOptions={availableFilters}
+            userFilterInputs={userFilterInputs}
+            userFilterInputsSetter={handleSetFilters}
+            userFilterReset={handleFilterReset}
+            filtersCounter={Number(userFilterInputs.filtersCounter[0])}
+            resultsCount={movieResults.length}
+            isUserConnected={isUserConnected.current}
+          />
+        )}
       </section>
       <section id="movie-grid">
         {error && <p>Une erreur est survenue.</p>}
         {!movieResults && !error && <p>Chargement des données.</p>}
-        {movieResults && 
-        <div className='flex flex-col gap-3 font-medium'>
-          <p className=''>{movieResults.length + ' films'}</p>
-          <ul className={gridStyle}>
-            {
-              movieResults.map((movie: CompleteMovie) => (
+        {movieResults && (
+          <div className="flex flex-col gap-3 font-medium">
+            <p className="">{movieResults.length + ' films'}</p>
+            <ul className={gridStyle}>
+              {movieResults.map((movie: CompleteMovie) => (
                 <li key={movie.french_title}>
                   <Link href={`/films/${movie.id}`}>
                     <Image
@@ -162,11 +177,11 @@ export default function Films() {
                       className={posterStyles}
                     />
                   </Link>
-                </li>))
-            }
-          </ul>
-        </div>
-        }
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </section>
     </main>
   );
