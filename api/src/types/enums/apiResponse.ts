d9d5f7b369a9d418249e768661523ec68cc9e048
BenoitSafari <gregory.michalak.192@gmail.com>
2022-12-29 01:55:19 +0100

    :pencil2: Changed messages contents on ApiError/Response

--- api/src/types/enums/apiResponse.ts
+++ api/src/types/enums/apiResponse.ts
@@ -6,8 +6,8 @@ export enum ApiResponse {
   PROPOSITION_UPDATE_SUCCESS = 'Votre proposition a bien été mise à jour.',
   PROPOSITION_PUBLICATION_SUCCESS = 'La proposition a bien été publiée.',
   DELETE_MOVIE_SUCCESS = 'Le film a bien été supprimée.',
-  DELETE_COMMENT_SUCCESS = 'Commentaire supprimé avec succés.',
-  DELETE_USER_SUCCESS = 'Utilisateur supprimé avec succés.',
+  DELETE_COMMENT_SUCCESS = 'Commentaire supprimé avec succès.',
+  DELETE_USER_SUCCESS = 'Utilisateur supprimé avec succès.',
   UPDATE_USER_SUCCESS = 'Votre profil a bien été mis à jour.',
   SEASON_CREATED_SUCCESS = 'Nouvelle saison créée.',
 }
