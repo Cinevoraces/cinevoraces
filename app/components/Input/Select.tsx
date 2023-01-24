c6c54c52f3c6cc2261335663d67ee0f6bc98b406
Julien GOLETTO <julien.goletto@gmail.com>
2023-01-24 11:10:04 +0100

    :zap: deleted custom hook on select inputs

--- app/components/Input/Select.tsx
+++ app/components/Input/Select.tsx
@@ -1,8 +1,5 @@
 import type { Dispatch, SetStateAction } from 'react';
-import { useRef } from 'react';
 import { Button, RadioInput } from '@components/Input';
-import useCloseMenuOnOutsideClick from '@hooks/useCloseMenuOnOutsideClick';
-import useCloseMenuOnEnterKeyPress from '@hooks/useCloseOnEnterPress';
 import { ArrowSvg } from '@components/SvgComponents';
 export interface OptionProps {
   name: string;
@@ -15,22 +12,29 @@ export interface SelectProps {
   displayOptionsSetter: ()=>void;
   stateValue: OptionProps;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  valueSetter: Dispatch<SetStateAction<OptionProps>> | ((option: OptionProps)=>{ payload: OptionProps; type: string; });
+  valueSetter:
+  | Dispatch<SetStateAction<OptionProps>>
+  | ((option: OptionProps)=>{ payload: OptionProps; type: string });
   selectCustomStyle?: 'searchbar';
 }
 
 const Select = (props: SelectProps) => {
-  const { name, options, displayOptionsState, displayOptionsSetter, stateValue, valueSetter, selectCustomStyle } = props;
+  const { name, options, displayOptionsState, displayOptionsSetter, stateValue, valueSetter, selectCustomStyle } =
+    props;
   const toggleSelectDisplay = () => displayOptionsSetter();
-  const selectRef = useRef<HTMLDivElement>(null);
-  useCloseMenuOnOutsideClick(selectRef, name, displayOptionsState, displayOptionsSetter);
-  useCloseMenuOnEnterKeyPress(displayOptionsState, displayOptionsSetter);
+  const handleCloseOptions = (e: React.MouseEvent) => {
+    displayOptionsSetter();
+  };
+  const handleCloseOnKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter' || e.key === 'Escape') displayOptionsSetter();
+  };
 
   return (
     <div
       id="select-input"
       className={name + ' relative flex flex-col gap-2 items-center'}
-      ref={selectRef}>
+      // ref={selectRef}
+    >
       {!selectCustomStyle ? (
         <Button
           customStyle="select"
@@ -56,25 +60,32 @@ const Select = (props: SelectProps) => {
         </button>
       )}
       {displayOptionsState && (
-        <fieldset
-          className={
-            name +
-            ` absolute z-20 top-12 w-full -ml-1 
-              text-sm 
-              py-2.5 flex flex-col gap-2 border rounded-xl 
-              bg-medium-gray border-orange-primary`
-          }>
-          {options.map((o) => (
-            <RadioInput
-              style="select"
-              key={o.name}
-              label={o.name}
-              name="season"
-              value={o.value}
-              onChange={() => valueSetter(o)}
-            />
-          ))}
-        </fieldset>
+        <>
+          <div
+            className="fixed top-0 left-0 z-0 w-screen h-screen bg-lime-800/10"
+            onClick={handleCloseOptions}
+          />
+          <fieldset
+            onKeyUp={handleCloseOnKeyPress}
+            className={
+              name +
+              ` absolute z-20 top-12 w-full -ml-1 
+                text-sm 
+                py-2.5 flex flex-col gap-2 border rounded-xl 
+                bg-medium-gray border-orange-primary`
+            }>
+            {options.map((o) => (
+              <RadioInput
+                style="select"
+                key={o.name}
+                label={o.name}
+                name="season"
+                value={o.value}
+                onChange={() => valueSetter(o)}
+              />
+            ))}
+          </fieldset>
+        </>
       )}
     </div>
   );
