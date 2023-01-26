0f1d40b55440f6b0b974692b832b36edc8292b84
Julien GOLETTO <julien.goletto@gmail.com>
2023-01-26 09:52:23 +0100

    :lipstick: Adding optionnal parameter to freeze pp naimation

--- app/components/Avatar/index.tsx
+++ app/components/Avatar/index.tsx
@@ -6,10 +6,11 @@ interface AvatarProps {
   url: string;
   id: number;
   pseudo: string;
+  interactive?: boolean;
 }
 
-const className = `w-[35px] h-[35px] p-0.5 border border-white rounded-full lg:w-[45px] lg:h-[45px]
-focus:outline-none focus:ring-4 focus:ring-offset-0 focus:ring-white/5 focus:scale-110 focus:border-2
+const baseStyle = 'w-[35px] h-[35px] p-0.5 border border-white rounded-full lg:w-[45px] lg:h-[45px]';
+const animationStyle = `focus:outline-none focus:ring-4 focus:ring-offset-0 focus:ring-white/5 focus:scale-110 focus:border-2
 hover:outline-none hover:ring-4 hover:ring-offset-0 hover:ring-white/5 hover:scale-110 hover:border-2
 transition duration-150 hover:ease-out`;
 
@@ -18,9 +19,10 @@ transition duration-150 hover:ease-out`;
  * @param url
  * @param id
  * @param pseudo
+ * @param interactive facultative param to freeze animation - default to true
  * @returns <Link> encapsulating the avatar picture of the concerned user
  */
-const Avatar = ({ url, id, pseudo }: AvatarProps) => {
+const Avatar = ({ url, id, pseudo, interactive }: AvatarProps) => {
   return (
     <Link href={`/membres/${id}`}>
       <Image
@@ -28,7 +30,7 @@ const Avatar = ({ url, id, pseudo }: AvatarProps) => {
         alt={`avatar de ${pseudo}`}
         height={50}
         width={50}
-        className={className}
+        className={(interactive === false) ? baseStyle : baseStyle + animationStyle}
       />
     </Link>
   );
