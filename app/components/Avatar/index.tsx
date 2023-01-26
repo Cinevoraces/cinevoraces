2b91fbfd6446578f1dd10355045a33b5ddd1a25b
Julien GOLETTO <julien.goletto@gmail.com>
2023-01-26 09:45:16 +0100

    :lipstick: Adding a hover-focus effect on all PP

--- app/components/Avatar/index.tsx
+++ app/components/Avatar/index.tsx
@@ -8,6 +8,11 @@ interface AvatarProps {
   pseudo: string;
 }
 
+const className = `w-[35px] h-[35px] p-0.5 border border-white rounded-full lg:w-[45px] lg:h-[45px]
+focus:outline-none focus:ring-4 focus:ring-offset-0 focus:ring-white/5 focus:scale-110 focus:border-2
+hover:outline-none hover:ring-4 hover:ring-offset-0 hover:ring-white/5 hover:scale-110 hover:border-2
+transition duration-150 hover:ease-out`;
+
 /**
  * 
  * @param url
@@ -23,7 +28,7 @@ const Avatar = ({ url, id, pseudo }: AvatarProps) => {
         alt={`avatar de ${pseudo}`}
         height={50}
         width={50}
-        className="user w-[35px] h-[35px] p-0.5 border border-white rounded-full lg:w-[45px] lg:h-[45px]"
+        className={className}
       />
     </Link>
   );
