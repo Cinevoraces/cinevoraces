69582918c3b8bb0193e873b6dda8f4950ddcb94c
Julien GOLETTO <julien.goletto@gmail.com>
2023-01-26 09:58:51 +0100

    :lipstick: Removed justify-between inside global container

--- app/components/Avatar/index.tsx
+++ app/components/Avatar/index.tsx
@@ -10,7 +10,7 @@ interface AvatarProps {
 }
 
 const baseStyle = 'w-[35px] h-[35px] p-0.5 border border-white rounded-full lg:w-[45px] lg:h-[45px]';
-const animationStyle = `focus:outline-none focus:ring-4 focus:ring-offset-0 focus:ring-white/5 focus:scale-110 focus:border-2
+const animationStyle = ` focus:outline-none focus:ring-4 focus:ring-offset-0 focus:ring-white/5 focus:scale-110 focus:border-2
 hover:outline-none hover:ring-4 hover:ring-offset-0 hover:ring-white/5 hover:scale-110 hover:border-2
 transition duration-150 hover:ease-out`;
 
