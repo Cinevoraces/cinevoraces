74017a757c5fae1fb9b26a29dfa57d23fe7575e3
Julien GOLETTO <juliengoletto@MacBook-Pro-de-Julien.local>
2023-03-02 13:26:46 +0100

    :bug: Fixed the avatar bug on comments section

--- app/pages_chunks/film/UI/CommentsSection.tsx
+++ app/pages_chunks/film/UI/CommentsSection.tsx
@@ -40,31 +40,27 @@ const CommentsSection = forwardRef<HTMLTextAreaElement, CommentsSectionProps>(({
     return [connectedUserComment[0], ...otherComments];
   };
 
-  // Using Ref to renew expansionState and allow freshly posted comment (just in cache) to be expanded as well as the others from API
-  const orderedComments = useRef<Comment[]>([]);
-  const cutComments = useRef<(string | boolean)[][]>([]);
+  // Using state to renew expansionState and allow freshly posted comment (just in cache) to be expanded as well as the others from API
+  const [orderedComments, setOrderedComments] = useState<Comment[]>([]);
+  const [cutComments, setCutComments] = useState<(string | boolean)[][]>([]);
+  const [commentsExpansionStates, setCommentsExpansionStates] = useState(comments.map((c: Comment) => false));
   useEffect(() => {
-    orderedComments.current = reorderComments(id, comments);
-    cutComments.current = orderedComments.current.map((c: Comment) => cutText(c.comment, 700));
-    setCommentsExpansionStates(orderedComments.current.map((c: Comment) => false));
+    setOrderedComments(reorderComments(id, comments));
+    setCutComments(orderedComments.map((c: Comment) => cutText(c.comment, 700)));
+    setCommentsExpansionStates(comments.map((c: Comment) => false));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id, comments]);
-  const [commentsExpansionStates, setCommentsExpansionStates] = useState(
-    orderedComments.current.map((c: Comment) => false)
-  );
-
   const toggleCommentExpansion = (index: number) => {
     const newState = commentsExpansionStates.map((ces: boolean, i: number) => (index === i ? !ces : ces));
     setCommentsExpansionStates(newState);
   };
 
   // Animation section
-  const trail = useTrail(
-    orderedComments.current.length, {
-      config: { mass: 1, tension: 300, friction: 36 },
-      from: { opacity:0, y:50 },
-      to: { opacity:100, y:0 }
-    }
-  );
+  const trail = useTrail(cutComments.length, {
+    config: { mass: 1, tension: 300, friction: 36 },
+    from: { opacity: 0, y: 50 },
+    to: { opacity: 100, y: 0 },
+  });
 
   return (
     <section
@@ -73,7 +69,7 @@ const CommentsSection = forwardRef<HTMLTextAreaElement, CommentsSectionProps>(({
       <h2 className="text-2xl font-semibold lg:text-3xl text-center">{`Commentaires (${comments.length})`}</h2>
       {
         // Add Comment button
-        id && orderedComments.current.filter((c) => c.author_id === id).length === 0 && !isCommentFormOpened && (
+        id && comments.filter((c) => c.author_id === id).length === 0 && !isCommentFormOpened && (
           <div className="flex justify-center">
             <Button
               customStyle="empty"
@@ -128,90 +124,89 @@ const CommentsSection = forwardRef<HTMLTextAreaElement, CommentsSectionProps>(({
       <div className="grid grid-cols-1 gap-4 md:gap-6 lg:gap-8 lg:grid-cols-2 justify-start  ">
         {
           // Displaying all published comments
-          orderedComments.current.length === 0 ? (
+          cutComments.length === 0 ? (
             <p className="text-center lg:col-span-2">Aucun commentaire pour ce film.</p>
           ) : (
             <>
-              {
-                trail.map((props, index) => (
-                  <animated.div style={props} key={index}>
-                    <PostCard
-                      type="comment"
-                      {...orderedComments.current[index]}>
-                      {
-                        // Go into edition mode
-                        isEditionFormOpened && id === orderedComments.current[index].author_id ? (
-                          <form
+              {trail.map((props, index) => (
+                <animated.div
+                  style={props}
+                  key={index}>
+                  <PostCard
+                    key={orderedComments[index].author_pseudo}
+                    type="comment"
+                    {...orderedComments[index]}>
+                    {
+                      // Go into edition mode
+                      isEditionFormOpened && id === orderedComments[index].author_id ? (
+                        <form
+                          id="comment-form"
+                          action="submit"
+                          className="flex flex-col gap-4"
+                          onSubmit={(e) => {
+                            onSubmit(e);
+                            toggleEditionForm();
+                          }}>
+                          <RefTextArea
                             id="comment-form"
-                            action="submit"
-                            className="flex flex-col gap-4"
-                            onSubmit={(e) => {
-                              onSubmit(e);
-                              toggleEditionForm();
-                            }}>
-                            <RefTextArea
-                              id="comment-form"
-                              ref={ref}
-                              defaultValue={orderedComments.current[index].comment}
-                            />
-                            <div
-                              id="comment-send-cancel"
-                              className="flex justify-end gap-4">
-                              <Button customStyle="rounded">
-                                <Image
-                                  src={SendLogo}
-                                  alt=""
-                                  width={16}
-                                  height={16}
-                                />
-                            Confirmer
-                              </Button>
-                              <Button
-                                onClick={toggleEditionForm}
-                                customStyle="rounded">
-                            Annuler
-                              </Button>
-                            </div>
-                          </form>
-                        ) : (
+                            ref={ref}
+                            defaultValue={orderedComments[index].comment}
+                          />
+                          <div
+                            id="comment-send-cancel"
+                            className="flex justify-end gap-4">
+                            <Button customStyle="rounded">
+                              <Image
+                                src={SendLogo}
+                                alt=""
+                                width={16}
+                                height={16}
+                              />
+                              Confirmer
+                            </Button>
+                            <Button
+                              onClick={toggleEditionForm}
+                              customStyle="rounded">
+                              Annuler
+                            </Button>
+                          </div>
+                        </form>
+                      ) : (
                         // If not, just display the comment, expanded or not
-                          <>
-                            <RichText>
-                              {
-                                // If the comment has a cut version & expanding state is true
-                                cutComments.current[index][1] && commentsExpansionStates[index]
-                                  ? orderedComments.current[index].comment as string
-                                  : cutComments.current[index][0] as string
-                              }
-                            </RichText>
+                        <>
+                          <RichText>
                             {
-                              // Adding button PostCard buttons
-                              (
-                                <div className="flex justify-end gap-4">
-                                  {id === orderedComments.current[index].author_id && (
-                                    <Button
-                                      onClick={toggleEditionForm}
-                                      customStyle="rounded">
-                                  Éditer
-                                    </Button>
-                                  )}
-                                  {orderedComments.current[index].comment.length > 700 && (
-                                    <Button
-                                      onClick={() => toggleCommentExpansion(index)}
-                                      customStyle="rounded">
-                                      {!commentsExpansionStates[index] ? 'Voir plus...' : 'Réduire'}
-                                    </Button>
-                                  )}
-                                </div>
-                              )
+                              // If the comment has a cut version & expanding state is true
+                              cutComments[index][1] && commentsExpansionStates[index]
+                                ? (orderedComments[index].comment as string)
+                                : (cutComments[index][0] as string)
                             }
-                          </>
-                        )
-                      }
-                    </PostCard>
-                  </animated.div>
-                ))      
-              }
+                          </RichText>
+                          {
+                            // Adding button PostCard buttons
+                            <div className="flex justify-end gap-4">
+                              {id === orderedComments[index].author_id && (
+                                <Button
+                                  onClick={toggleEditionForm}
+                                  customStyle="rounded">
+                                  Éditer
+                                </Button>
+                              )}
+                              {orderedComments[index].comment.length > 700 && (
+                                <Button
+                                  onClick={() => toggleCommentExpansion(index)}
+                                  customStyle="rounded">
+                                  {!commentsExpansionStates[index] ? 'Voir plus...' : 'Réduire'}
+                                </Button>
+                              )}
+                            </div>
+                          }
+                        </>
+                      )
+                    }
+                  </PostCard>
+                </animated.div>
+              ))}
             </>
           )
         }
