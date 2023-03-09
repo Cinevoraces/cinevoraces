bda3a6178b319000214677bd085f506d38fa3a8b
Julien GOLETTO <juliengoletto@MacBook-Pro-de-Julien.local>
2023-03-09 17:20:11 +0100

    :test_tube: test on relative path to logo

--- app/components/Head/index.tsx
+++ app/components/Head/index.tsx
@@ -24,7 +24,7 @@ const CustomHead = ({ title, description, slug, imageUrl }: HeadProps) => {
       <meta property="og:type" content='website'/>
       <meta property="og:title" content={title}/>
       <meta property="og:description" content={description}/>
-      <meta property="og:image" content={(imageUrl) ? imageUrl : '/mstile-150x150.png'}/>
+      <meta property="og:image" content={(imageUrl) ? imageUrl : 'mstile-150x150.png'}/>
       <meta name="theme-color" content="#202029"/>
       <meta name="twitter:card" content="summary_large_image"></meta>
     </Head>
