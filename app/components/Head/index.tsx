b89b9ef9c9fe3b3131f4085e417a061d9ae337ec
Julien GOLETTO <juliengoletto@MacBook-Pro-de-Julien.local>
2023-03-09 17:06:09 +0100

    :test_tube: tests on head

--- app/components/Head/index.tsx
+++ app/components/Head/index.tsx
@@ -25,6 +25,8 @@ const CustomHead = ({ title, description, slug, imageUrl }: HeadProps) => {
       <meta property="og:title" content={title}/>
       <meta property="og:description" content={description}/>
       <meta property="og:image" content={(imageUrl) ? imageUrl : '/mstile-150x150.png'}/>
+      <meta name="them-color" content="#202029"/>
+      <meta name="twitter:card" content="summary_large_image"></meta>
     </Head>
   );
 };
