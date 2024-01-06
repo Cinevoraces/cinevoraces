2ccf4b6187720fd022aae24154d2d11c14a9da39
benoitSafari <gregory.michalak.192@gmail.com>
2024-01-06 23:36:45 +0100

    restore sh script update

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-echo 'Please enter a backup name, without the .tar extension. The tar archive must be placed inside data/backups folder, on host.'
-read
-source .env && cd data/backups
-echo "Unzipping backup archive..."
-tar -xvf ./$REPLY.tar 
-docker exec api rm -rf public
-docker cp ./$REPLY/public api:/api
-docker exec postgres pg_restore -c --no-owner -v -U ${POSTGRES_USER} -d ${POSTGRES_DB} data/backups/$REPLY/database_$REPLY
-rm -rf $REPLY
-echo 'Backup restore achieved.'
+backup_files=$(ls ./data/backups)
+source ./data/.env
+
+if [ -z "$backup_files" ]; then
+    echo "No backup files found."
+    return 0
+fi
+
+echo "Please select a backup file to restore:"
+
+select backup_file in $backup_files Cancel; do
+    case $backup_file in
+        Cancel )
+            echo "Operation cancelled."
+            exit
+            ;;
+        * )
+            if [ -n "$backup_file" ]; then
+                tar -xvf "./data/backups/${backup_file}" -C ./data/backups
+                sudo docker stop api
+                sudo docker stop app
+                sudo docker exec api rm -rf public
+                sudo docker cp "./data/backups/${backup_file%.*}/public" api:/api/public
+                sudo docker exec postgres pg_restore -c --no-owner -v -U ${POSTGRES_USER} -d ${POSTGRES_DB} "./data/backups/${backup_file%.*}/database_${backup_file%.*}"
+                sudo docker start api
+                sudo docker start app
+                rm -rf "./data/backups/${backup_file%.*}"
+                echo "Database restored."
+                exit
+            fi
+            ;;
+    esac
+done
