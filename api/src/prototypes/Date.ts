9b73db1f4722fa4fcd9c80e8869e69467aaa8313
BenoitSafari <gregory.michalak.192@gmail.com>
2024-01-16 23:32:43 +0100

    fixed date error

--- api/src/prototypes/Date.ts
+++ api/src/prototypes/Date.ts
@@ -1,13 +1,13 @@
-type NamedDay = 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday';
+type NamedDay = 'sunday' | 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday';
 
 const NamedDays: Record<NamedDay, number> = {
-    saturday: 0,
-    sunday: 1,
-    monday: 2,
-    tuesday: 3,
-    wednesday: 4,
-    thursday: 5,
-    friday: 6,
+    sunday: 0,
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6,
 };
 
 Object.assign(Date, {
