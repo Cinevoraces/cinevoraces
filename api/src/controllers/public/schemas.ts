f46d0d03a5ef1ef486d7d1490ada15c357cd09f4
BenoitSafari <gregory.michalak.192@gmail.com>
2024-01-21 19:51:33 +0100

    Integration tests + fixes: Public API

--- api/src/controllers/public/schemas.ts
+++ api/src/controllers/public/schemas.ts
@@ -2,7 +2,7 @@ export default [
     {
         $id: 'API:GET/public/:docType/:entityId',
         description:
-            '**Get a file using it\'s entity\'s id:** For an avatar use the parameter "avatar" followed by the corresponding userId. For a movie poster use the parameter "poster" followed by the corresponding movieId.',
+            '**Get a file using it\'s entity\'s id:** For an avatar use the parameter "0" followed by the corresponding userId. For a movie poster use the parameter "1" followed by the corresponding movieId.',
         tags: ['Public'],
         params: {
             type: 'object',
