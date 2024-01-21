7334506038ac537f28ac509449c836a7e9b7edf3
BenoitSafari <gregory.michalak.192@gmail.com>
2024-01-21 17:19:41 +0100

    Updated dev scripts

--- api/scripts/dev.mjs
+++ api/scripts/dev.mjs
@@ -1,4 +1,4 @@
-import { exec } from 'child_process';
+import nodemon from 'nodemon';
 import { replaceTscAliasPaths } from 'tsc-alias';
 import { TscWatchClient } from 'tsc-watch/client.js';
 
@@ -9,27 +9,17 @@ import { TscWatchClient } from 'tsc-watch/client.js';
  * - nodemon is used to restart the server when a file changes (with debug on port :9229).
  */
 
+const nodemonCfg = '--inspect dist/src/index.js 9229';
+
 const watch = new TscWatchClient();
 
-watch.on('started', () => console.log('Starting compilation...'));
+watch.on('started', () => void 0);
 
 watch.on('first_success', () => {
     replaceTscAliasPaths();
-
-    exec('npx nodemon --inspect -r ts-node/register dist/src/index.js', (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-    });
+    nodemon(nodemonCfg);
 });
 
 watch.on('success', () => replaceTscAliasPaths());
-
 watch.on('compile_errors', () => {});
-
 watch.start('--project', '.');
