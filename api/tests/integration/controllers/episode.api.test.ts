d5521c8a007a627624f258790dbdceb295daf1d2
BenoitSafari <gregory.michalak.192@gmail.com>
2024-01-21 20:01:36 +0100

    Integration tests + fixes: Episodes API

--- api/tests/integration/controllers/episode.api.test.ts
+++ api/tests/integration/controllers/episode.api.test.ts
@@ -1,1 +1,23 @@
-// TODO: On user_refacto merge
+import type { InjectOptions } from 'fastify';
+import { buildTestServer } from '../../utils/buildTestServer';
+import { logUser } from '../../utils/logUser';
+
+describe('API - Review controller', () => {
+    const server = buildTestServer();
+    let userHeaders: InjectOptions;
+
+    beforeAll(async () => {
+        server.fastify.ready();
+        userHeaders = await logUser(server.fastify);
+    });
+    afterAll(() => server.fastify.close());
+
+    it('API:GET/episodes', async () => {
+        const { statusCode } = await server.fastify.inject({
+            method: 'GET',
+            url: '/episodes',
+            ...userHeaders,
+        });
+        expect(statusCode).toBe(200);
+    });
+});
