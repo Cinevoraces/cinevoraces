49e859dfa91c1247814f6bdaf19593a1899b1b1d
benoitsafari <gregory.michalak.192@gmail.com>
2024-01-25 12:33:47 +0100

    Movie integration tests

--- api/tests/integration/controllers/movie.api.test.ts
+++ api/tests/integration/controllers/movie.api.test.ts
@@ -1,20 +1,16 @@
 import { buildTestServer } from '../../utils/buildTestServer';
-import { logUser } from '../../utils/logUser';
 
 describe('API - Movie controller', () => {
     const server = buildTestServer();
-    let userHeaders = {};
 
-    beforeAll(async () => {
-        server.fastify.ready();
-        userHeaders = await logUser(server.fastify);
-    });
+    beforeAll(async () => server.fastify.ready());
     afterAll(() => server.fastify.close());
 
     it('API:GET/movies - should public movies', async () => {
         const { statusCode, json } = await server.fastify.inject({
             method: 'GET',
             url: '/movies',
+            query: '?where[is_published]=true',
         });
 
         const res: Array<Record<string, unknown>> = await json();
@@ -24,5 +20,17 @@ describe('API - Movie controller', () => {
         for (const m of res) {
             expect(m.is_published).toBeTruthy();
         }
+    });
+
+    it('API:GET/movies/random-posters/:count', async () => {
+        const { statusCode, json } = await server.fastify.inject({
+            method: 'GET',
+            url: '/movies/random-posters/5',
+        });
+
+        const res: Array<Record<string, unknown>> = await json();
+
+        expect(statusCode).toEqual(200);
+        expect(res.length).toEqual(5);
     });
 });
