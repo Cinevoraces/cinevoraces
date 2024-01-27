cae7f2f784f847a95a015821422469c2645290e3
BenoitSafari <gregory.michalak.192@gmail.com>
2024-01-27 20:28:19 +0100

    Avatar hotfix

--- api/src/services/PublicService.ts
+++ api/src/services/PublicService.ts
@@ -9,26 +9,27 @@ export default class PublicService extends Service {
      * Get document by entity type and id
      */
     getDocumentById: GetDocumentByIdFn = async (type, entityId) => {
-        // FIXME: API parameter should directly ask for EDocType
-        const tableName = [
-            { type: EDocType.AVATAR, table: 'user' },
-            { type: EDocType.POSTER, table: 'movie' },
+        const table = [
+            { type: EDocType.AVATAR, table: 'public.user' },
+            { type: EDocType.POSTER, table: 'public.movie' },
         ].find(({ type: t }) => t === type)?.table;
 
-        if (!tableName)
-            throw new ServerError(400, 'INVALID_DOC_TYPE', 'Invalid document type, should be "avatar" or "poster"');
+        if (!table)
+            throw new ServerError(
+                400,
+                'INVALID_DOC_TYPE',
+                'Invalid document type, should be "avatar" (0) or "poster" (1)',
+            );
 
-        // Get the file path
         const { rows, rowCount } = await this.postgres.query<{ filename: string; content_type: string }>({
             text: ` 
                     SELECT filename, content_type FROM document 
-                    WHERE type = $1 
-                    AND document_group_id = 
-                    (SELECT document_group_id FROM ${tableName} WHERE id = $2);`,
+                    WHERE type = $1 AND document_group_id = 
+                    (SELECT document_group_id FROM ${table} WHERE id = $2);`,
             values: [type, entityId],
         });
+        if (!rowCount) throw new ServerError(404, 'NOT_FOUND'); // issues/168
 
-        if (!rowCount) throw new ServerError(404, 'NOT_FOUND', 'Requested document does not exist'); // issues/168
-        return `${getFolderPath('public')}/${type}${entityId}.${rows[0].content_type.split('/')[1]}`;
+        return `${getFolderPath('public')}/${rows[0].filename}.${rows[0].content_type.split('/')[1]}`;
     };
 }
