091fedeb5c3e93f56673c293f4598439fb6bc789
BenoitSafari <gregory.michalak.192@gmail.com>
2024-02-03 22:19:47 +0100

    removed useless template

--- api/src/templates/index.ts
+++ api/src/templates/index.ts
@@ -1,5 +1,4 @@
 import confirmMail from './email/confirmMail';
-import register from './email/register';
 import resetPassword from './email/resetPassword';
 
-export const templates = { register, confirmMail, resetPassword };
+export const templates = { confirmMail, resetPassword };
