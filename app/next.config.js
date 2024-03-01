f8acbe387eeddf4dd7deaee63c9a04909b58cb55
jgoletto <julien.goletto@ose-engineering.fr>
2024-03-01 18:02:51 +0100

    :twisted_rightwards_arrows: Updated changes from #182

--- app/next.config.js
+++ app/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
-    domains: ['image.tmdb.org', 'res.cloudinary.com', 'cinevoraces_api'],
+    domains: ['localhost', 'image.tmdb.org', 'res.cloudinary.com', 'cinevoraces_api'],
   },
   env: {
     NEXT_PUBLIC_TMDB_KEY: process.env.TMDB_KEY,
@@ -13,9 +13,9 @@ const nextConfig = {
     NEXT_PUBLIC_API_BASE_URL_SSR: process.env.API_BASE_URL_SSR,
     NEXT_PUBLIC_DISCORD_INVITE_URL: process.env.DISCORD_INVITE_URL,
     // For some reason .env.local doesn't accept a classic regex pattern such as in ordinary .env
-    NEXT_PUBLIC_PASS_REGEXP: "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!#$%*+=?|-]).{12,}$",
-    NEXT_SHARP_PATH: '/tmp/node_modules/sharp'
-  }
-}
+    NEXT_PUBLIC_PASS_REGEXP: '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!#$%*+=?|-]).{12,}$',
+    NEXT_SHARP_PATH: '/tmp/node_modules/sharp',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
